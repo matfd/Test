a6c68d722a4994227cac28f65bb157fc3c03ad5a
matfd <S2a0s0h5a01122005@gmail.com>
2020-11-24 17:14:59 +0300

    Oh shit

---
+++ Osnova.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+	ofstream outFile;
+	outFile.open("my_file.txt");
+	string str;
+	getline(cin, str);
+	outFile << str;
+	outFile.close();
+	return 0;
+}
